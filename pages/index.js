8c40c730d11d4855a85bd5b973c4ac1cc39a05ff
Tanvir Mahin <55342128+TanvirMahin24@users.noreply.github.com>
2025-02-15 15:32:01 +0600

    Revert "Optimization PR"

--- pages/index.js
+++ pages/index.js
@@ -1,44 +1,19 @@
 import Head from "next/head";
-// import dynamic from "next/dynamic";
 import { Fragment } from "react";
-import Home from "@containers/home";
-import Script from "next/script";
-import { gameData } from "data/game";
+import Home from "../containers/home";
 
-export default function Homepage({ games }) {
+export default function Homepage() {
   return (
     <Fragment>
       <Head>
-        <title>Fin Games</title>
+        <title>{"Fin Games"}</title>
         <link rel="icon" href="/gameImage/launch-icon.png" />
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1, shrink-to-fit=no"
-        />
-        <meta
-          name="description"
-          content="Fin Games - The best place for gaming."
-        />
-        <meta name="author" content="Fin Games" />
-        <Script
-          async
-          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4575195873243785"
-          crossOrigin="anonymous"
-        ></Script>
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4575195873243785"
+     crossorigin="anonymous"></script>
       </Head>
 
-      <Home games={games} />
+        <Home />
+
     </Fragment>
   );
-}
-
-export async function getStaticProps() {
-  // Fetch game data (or import from a static file)
-  const games = gameData; // Use static import or API fetch if needed
-  return {
-    props: {
-      games,
-    },
-    revalidate: 86400, // Regenerate the page every 1 days
-  };
 }
