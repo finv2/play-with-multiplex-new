5c963f138c797f1a28f9c43a02a46b699b29d44c
TanvirMahin24 <tanvirmahin24@gmail.com>
2025-02-17 00:11:45 +0600

    Optimization v2

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,33 @@
 import Head from "next/head";
 import { Fragment } from "react";
 import Home from "../containers/home";
+import { gameData } from "data/game";
 
-export default function Homepage() {
+export default function Homepage({ games }) {
   return (
     <Fragment>
       <Head>
         <title>{"Fin Games"}</title>
         <link rel="icon" href="/gameImage/launch-icon.png" />
-        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4575195873243785"
-     crossorigin="anonymous"></script>
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4575195873243785"
+          crossorigin="anonymous"
+        ></script>
       </Head>
 
-        <Home />
-
+      <Home games={games} />
     </Fragment>
   );
+}
+
+export async function getStaticProps() {
+  // Fetch game data (or import from a static file)
+  const games = gameData; // Use static import or API fetch if needed
+  return {
+    props: {
+      games,
+    },
+    revalidate: 86400, // Regenerate the page every 1 days
+  };
 }
