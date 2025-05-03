f63ec3a94de7ec8b6063f783e3129aa0ab625945
finv2 <v2@fingameon.com>
2025-05-03 18:18:41 +0530

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -26,10 +26,10 @@ export default function Document() {
           dangerouslySetInnerHTML={{
             __html: `
               (function(c,l,a,r,i,t,y){
-                  c[a]=c[a]function(){(c[a].q=c[a].q[]).push(arguments)};
+                  c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
                   t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
                   y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-              })(window, document, "clarity", "script", "rdrgupgmsh");
+              })(window, document, "clarity", "script", "rds8834tpj");
             `,
           }}
         />
