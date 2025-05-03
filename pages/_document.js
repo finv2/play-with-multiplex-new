939033868eac32bccf8858ccc6f7a5c74c6c9edc
finv2 <v2@fingameon.com>
2025-05-03 17:18:45 +0530

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -21,6 +21,18 @@ export default function Document() {
             href="https://cdn.jsdelivr.net/npm/neuicons/css/neu.min.css"
           />
         </noscript>
+              <script
+          type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(c,l,a,r,i,t,y){
+                  c[a]=c[a]function(){(c[a].q=c[a].q[]).push(arguments)};
+                  t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                  y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "rdrgupgmsh");
+            `,
+          }}
+        />
       </Head>
 
       <body>
